bb5297a4bdf61cc252f2a1bf60757ae63763f89b
Takuiya-dev <example@gmail.com>
2023-10-03 11:03:42 +0900

    Finished SVG Refactoring

--- Components/styles/SidebarButtonStyles.js
+++ Components/styles/SidebarButtonStyles.js
@@ -1,26 +1,25 @@
 import { css } from '../../lit.js';
 
 const SidebarButtonStyles = css`
-:host,
-			div {
-				font-family: Barlow;
-				font-size: 20px;
-				font-style: normal;
-				font-weight: var(--regular-weight)
-				line-height: 20px;
-				display: inline-flex;
-				align-items: center;
-				gap: 12px;
-				padding: 12px;
-				border-radius: 50px;
-				cursor: pointer;
-			}
+	:host,
+	div {
+		display: inline-flex;
+		align-items: center;
+		gap: 12px;
+		padding: 12px;
+		border-radius: 50px;
+		font-size: 20px;
+		font-style: normal;
+		font-weight: var(--regular-weight);
+		line-height: 20px;
+		letter-spacing: 0px;
+		cursor: pointer;
+	}
 
-			:host p {
-				padding: 0;
-				margin: 0;
-				text-transform: capitalize;
-			}
+	:host p {
+		padding: 0;
+		margin: 0;
+	}
 `;
 
 export default SidebarButtonStyles;
